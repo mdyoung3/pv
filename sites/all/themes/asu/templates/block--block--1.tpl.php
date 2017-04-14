a3531f3141558099739c0bc88c18734c46ac4cae
mdyoung3 <mdyoung3@asu.edu>
2017-04-14 16:12:17 +0000

    Updated the footer to have the address.

--- sites/all/themes/asu/templates/block--block--1.tpl.php
+++ sites/all/themes/asu/templates/block--block--1.tpl.php
@@ -2,14 +2,15 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6 col-sm-12 col-xs-12">
-				<h2>LOGO</h2>
-				<h1>Knowledge Enterprise Development</h1>
-				<div class="address">1000 S. Forest Mall</div>
-				<div class="address">Tempe, AZ 85281<br /> &nbsp; </div>
-				<p>Phone: 480-884-1860 | Fax: 480-884-1888</p>
+
+				<h1>Solar Power Laboratory</h1>
+                <h2>Arizona State University Research Park</h2>
+				<div class="address">7700 South River Parkway</div>
+				<div class="address">Tempe, Arizona 85284-1808<br /> &nbsp; </div>
+                <p><a href="mailto:pv@asu.edu">pv@asu.edu</a></p>
 				<div>
 					<ul class="social-media">
-						<li><a href="https://www.facebook.com/" target="_blank" title="Facebook"><span class="sr-only">Facebook</span><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+						<!--<li><a href="https://www.facebook.com/" target="_blank" title="Facebook"><span class="sr-only">Facebook</span><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.twitter.com/" target="_blank" title="Twitter"><span class="sr-only">Twitter</span><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.linkedin.com/" target="_blank" title="LinkedIn"><span class="sr-only">LinkedIn</span><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.instagram.com/" target="_blank" title="Instagram"><span class="sr-only">Instagram</span><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
@@ -17,7 +18,7 @@
 						<li><a href="https://www.pinterest.com/" target="_blank" title="Pinterest"><span class="sr-only">Pinterest</span><i class="fa fa-pinterest-square" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.google.com/" target="_blank" title="Google Plus"><span class="sr-only">Google+</span><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
 						<li><a href="https://www.youtube.com/" target="_blank" title="YouTube"><span class="sr-only">YouTube</span><i class="fa fa-youtube-square" aria-hidden="true"></i></a></li>
-						<li><a href="https://www.vimeo.com/" target="_blank" title="Vimeo"><span class="sr-only">Vimeo</span><i class="fa fa-vimeo-square" aria-hidden="true"></i></a></li>
+						<li><a href="https://www.vimeo.com/" target="_blank" title="Vimeo"><span class="sr-only">Vimeo</span><i class="fa fa-vimeo-square" aria-hidden="true"></i></a></li>-->
 					</ul><!-- .social-media -->
 				</div>
 			</div><!-- .col -->
@@ -33,10 +34,8 @@
 			<div class="col-md-3 col-sm-6 col-xs-12">
 				<h3>About</h3>
 				<ul>
-					<li><a href="#link">Contact Us</a></li>
-					<li><a href="#link">Technology</a></li>
-					<li><a href="#link">Grant Support</a></li>
-					<li><a href="#link">People</a></li>
+					<li><a href="/people">About Us</a></li>
+					<li><a href="/visitors">Visitors</a></li>
 				</ul>
 			</div><!-- .col -->
 		</div><!-- .row -->
