099f910746464e677d903c682559e60b025da91e
Marc Young <mdyoung3@asu.edu>
2017-10-09 22:51:39 -0700

    Delete self.alias.drushrc.php

--- .drush/self.alias.drushrc.php
+++
@@ -1,9 +0,0 @@
-<?php  
-// Site mysite.com, environment local i
-$aliases['mysite.local'] = array( 
-  'root' => '/my/local/file/path/drupal', 
-  'uri' => 'mylocalsite', 
-  'path-aliases' => array( 
-    '%files' => 'sites/default/files', 
-  ) 
-); 
